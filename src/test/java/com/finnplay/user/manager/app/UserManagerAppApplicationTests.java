6a3448246cdfac225acbeb9db061c9787ff304d6
Danila Galimov <danilagalimov@gmail.com>
2022-11-19 20:43:44 +0200

    Change DB to H2 for context tests

--- src/test/java/com/finnplay/user/manager/app/UserManagerAppApplicationTests.java
+++ src/test/java/com/finnplay/user/manager/app/UserManagerAppApplicationTests.java
@@ -1,9 +1,11 @@
 package com.finnplay.user.manager.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class UserManagerAppApplicationTests {
 
 	@Test
